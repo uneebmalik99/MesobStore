5db8bc9e69af24c335bc1b846e0fbe4ff2559ff8
Uneeb Malik <76760539+uneebmalik99@users.noreply.github.com>
2023-10-04 18:18:48 +0500

    s

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,14 +8,16 @@ module.exports = {
     production : {
       key : 'mesob.pem' ,
       user : 'ubuntu',
-      host : '3.86.164.117',
+      host : '184.73.35.158',
       ref  : 'origin/main',
       repo : 'https://github.com/uneebmalik99/MesobStore.git',
       path : '/home/ubuntu',
       'pre-deploy-local': '',
-      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
   }
 };
+
+
