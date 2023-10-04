5ebe61a327674eb2c62f0b09fe1056c0d8b48e9e
Uneeb Malik <76760539+uneebmalik99@users.noreply.github.com>
2023-10-04 15:32:21 +0500

    c-11

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       user : 'ubuntu',
       host : '3.86.164.117',
       ref  : 'origin/main',
-      repo : 'git@github.com:uneebmalik99/MesobStore.git',
+      repo : 'https://github.com/uneebmalik99/MesobStore.git',
       path : '/home/ubuntu',
       'pre-deploy-local': '',
       'post-deploy' : 'source ~/.nvm/.nmv.sh && npm install && npm run build pm2 reload ecosystem.config.js --env production',
