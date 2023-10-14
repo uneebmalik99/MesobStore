7492f0f827f03cf5706ac3e65f6c9cdc1a268383
Uneeb Malik <76760539+uneebmalik99@users.noreply.github.com>
2023-10-14 10:57:15 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
-
 module.exports = {
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-    
-      ignoreDuringBuilds: true,
-      terser: false,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+  
+    ignoreDuringBuilds: true,
+    output: 'export',
+    swcMinify : false,
+    terser: false,
+    optimizeFonts: true,
 
-    },
+  },
 }
