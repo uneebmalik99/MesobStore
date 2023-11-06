c876dbf53da3f92583eac6846cc84f646e640a49
Uneeb Malik <76760539+uneebmalik99@users.noreply.github.com>
2023-11-07 00:21:56 +0500

    Update index.js

--- src/components/Checkout/index.js
+++ src/components/Checkout/index.js
@@ -39,9 +39,11 @@ function Checkout({ checkoutItems }) {
 //   const STRIPE_SK_EU =loadStripe
 //     ('sk_test_51Ma0UlHlGffSuHzfXqLxMCx4WwZPl2InuHG7TFmdFPczonVev6xnsrQzyJ0QkiCNP04yyUMiJGGnt8XXWiWEmAG700oh8MwmIz');
     
-  let STRIPE_PK_GLOBAL =loadStripe('pk_test_51KZzWbAhBlpHU9kBF7mHsYqqk6Ma8MGqjS9PB2pfwRcSW9npj1fv3YCqsFOESqTYvzoGIdBuZ9y3qKpTkhwpc9TO00kMQrezA4');
-  let STRIPE_PK_EU =loadStripe('pk_test_51Ma0UlHlGffSuHzfQ0MLtY2NxXXevZvjKNBMh1gLgrHedV5ZqbTvX8aLAFQC4YaFmdAlwUVmhjrcCevWbopcfHNQ00c9HutQd3');
-  
+// let STRIPE_PK_GLOBAL =loadStripe('');
+// let STRIPE_PK_EU =loadStripe('');
+let STRIPE_PK_GLOBAL =loadStripe('pk_test_51KZzWbAhBlpHU9kBF7mHsYqqk6Ma8MGqjS9PB2pfwRcSW9npj1fv3YCqsFOESqTYvzoGIdBuZ9y3qKpTkhwpc9TO00kMQrezA4');
+let STRIPE_PK_EU =loadStripe('pk_test_51Ma0UlHlGffSuHzfQ0MLtY2NxXXevZvjKNBMh1gLgrHedV5ZqbTvX8aLAFQC4YaFmdAlwUVmhjrcCevWbopcfHNQ00c9HutQd3');
+
   // // TEST -- PRODUCTION
 //   let STRIPE_SK_GLOBAL =loadStripe
 //     ('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');
@@ -323,9 +325,9 @@ const childRef = useRef();
                 const data = JSON.parse(res.data);
                 console.log("hgfyc"+JSON.stringify(data)); 
                 // if(region_1 == 'eu'){
-                //     STRIPE_PK_EU = loadStripe(data.publishableKey);
+                //     STRIPE_PK_EU = 'loadStripe('+data.publishableKey+')';
                 // }else{
-                //     STRIPE_PK_GLOBAL = loadStripe(data.publishableKey);
+                //     STRIPE_PK_GLOBAL = 'loadStripe('+data.publishableKey+')';
 
                 // }
               
